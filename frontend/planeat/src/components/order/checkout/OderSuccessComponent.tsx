aba8ae462036c186d5a90cacbfc50b7d28fa61f7
marcosteiner <marco.steiner@windowslive.com>
2021-02-24 10:40:59 +0100

    Added text to success component

--- frontend/planeat/src/components/order/checkout/OderSuccessComponent.tsx
+++ frontend/planeat/src/components/order/checkout/OderSuccessComponent.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import {Typography} from "@material-ui/core";
 
 export default function OrderSuccessComponent() {
-  return <></>
+  return <>
+    <Typography>
+      Bestellung Erfolgreich!
+      Du kannst dein essen noch bis 18:00 Uhr absagen.
+      Wir benachritigen dich per SMS sobald wir beginnen und wann du es abholen kannst.
+    </Typography>
+  </>
 }
