23a075e9bfdb168d5b79826f55b7040582aa4718
marcosteiner <marco.steiner@windowslive.com>
2021-02-24 19:44:19 +0100

    Changed dish image for prototype

--- frontend/planeat/src/components/order/planning/DishListComponent.tsx
+++ frontend/planeat/src/components/order/planning/DishListComponent.tsx
@@ -3,12 +3,16 @@ import DishComponent from "./DishComponent";
 import {DishModel} from "../../../models/DishModel";
 
 export default function DishListComponent() {
-  let dish = new DishModel('Acai Bowl', 200, 10.00, '/static/images/acai_bowl.jpg')
+  let dish1 = new DishModel('Acai Bowl', 200, 10.00, '/static/images/acai_bowl.jpg')
+  let dish2 = new DishModel('Früchte Bowl', 200, 10.00, '/static/images/fruit_bowl.jpg')
+  let dish3 = new DishModel('Massman Curry', 200, 10.00, '/static/images/massman_curry.jpg')
+  let dish4 = new DishModel('Rotes Thai Curry', 200, 10.00, '/static/images/red_thai_curry.jpg')
+  let dish5 = new DishModel('Tofu Stir Fry', 200, 10.00, '/static/images/tofu_stir_fry.jpg')
   return <>
-    <DishComponent dish={dish}/>
-    <DishComponent dish={dish}/>
-    <DishComponent dish={dish}/>
-    <DishComponent dish={dish}/>
-    <DishComponent dish={dish}/>
+    <DishComponent dish={dish1}/>
+    <DishComponent dish={dish2}/>
+    <DishComponent dish={dish3}/>
+    <DishComponent dish={dish4}/>
+    <DishComponent dish={dish5}/>
   </>
 }
