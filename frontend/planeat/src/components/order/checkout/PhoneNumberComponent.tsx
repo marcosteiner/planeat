59e66b076332f32e2d131211b65de7b7a663f09e
marcosteiner <marco.steiner@windowslive.com>
2021-02-24 19:17:47 +0100

    Added formik and rewrote phone number form

--- frontend/planeat/src/components/order/checkout/PhoneNumberComponent.tsx
+++ frontend/planeat/src/components/order/checkout/PhoneNumberComponent.tsx
@@ -1,11 +1,60 @@
 import React from 'react';
-import {TextField, Typography} from "@material-ui/core";
+import {Button, Paper, TextField, Typography} from "@material-ui/core";
+import {useHistory} from "react-router-dom";
+import { withFormik, FormikProps, FormikErrors, Form, Field } from 'formik';
+
+interface FormValues {
+  phoneNumber: string;
+}
+
+interface OtherProps {
+}
 
 export default function PhoneNumberComponent() {
-  return <div>
-    <Typography>Bestätige deine Bestellung mit deiner Telefonnummer</Typography>
-    <form noValidate autoComplete="off">
-      <TextField id="outlined-basic" label="Telefonnummer" variant="outlined" />
-    </form>
-  </div>
+  return <Paper>
+    <PhoneNumberForm/>
+  </Paper>
 }
+
+const InnerForm = (props: OtherProps & FormikProps<FormValues>) => {
+  const { touched, errors, isSubmitting } = props;
+  return (
+    <Form>
+      <Typography>Bestätige deine Bestellung mit deiner Telefonnummer</Typography>
+      <TextField name="phoneNumber" variant="outlined" placeholder="Telefonnummer" fullWidth/>
+      {touched.phoneNumber && errors.phoneNumber && <div>{errors.phoneNumber}</div>}
+
+      <Button variant="contained" color="primary" type="submit" disabled={isSubmitting}>
+        Bestätigungscode senden
+      </Button>
+    </Form>
+  );
+};
+
+// The type of props MyForm receives
+interface MyFormProps {
+  initialPhoneNumber?: string;
+}
+
+const PhoneNumberForm = withFormik<MyFormProps, FormValues>({
+  // Transform outer props into form values
+  mapPropsToValues: props => {
+    return {
+      phoneNumber: props.initialPhoneNumber || '',
+    };
+  },
+
+  // Add a custom validation function (this can be async too!)
+  validate: (values: FormValues) => {
+    let errors: FormikErrors<FormValues> = {};
+    if (!values.phoneNumber) {
+      errors.phoneNumber = 'Telefonnummer eingeben';
+    }
+    return errors;
+  },
+
+  handleSubmit: values => {
+    // do submitting things
+  },
+
+})(InnerForm);
