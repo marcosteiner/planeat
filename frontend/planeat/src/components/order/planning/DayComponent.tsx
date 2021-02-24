233ecdd2c17b7eaf8cdfa2cf555d9f8d88862166
marcosteiner <marco.steiner@windowslive.com>
2021-02-24 17:42:12 +0100

    Adjustments to day component

--- frontend/planeat/src/components/order/planning/DayComponent.tsx
+++ frontend/planeat/src/components/order/planning/DayComponent.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {DayModel} from "../../../models/DayModel";
-import {Box, Card, makeStyles, Typography} from "@material-ui/core";
+import {Box, Card, makeStyles, Paper, Typography} from "@material-ui/core";
 import {DishModel} from "../../../models/DishModel";
 import {useHistory} from "react-router-dom";
 
 const useStyles = makeStyles({
   dayCard: {
     marginBottom: '5px',
+    padding: '5px'
   },
+  mealPaper: {
+    marginBottom: '5px'
+  }
 });
 
 export default function DayComponent(props: {day: DayModel}) {
@@ -15,9 +19,10 @@ export default function DayComponent(props: {day: DayModel}) {
   let day = props.day
   return <>
     <Card className={classes.dayCard}>
-      <Meal mealName={"Morgenessen:"} dish={day.breakfast}/>
-      <Meal mealName={"Mittagessen:"} dish={day.lunch}/>
-      <Meal mealName={"Abendessen:"} dish={day.dinner}/>
+      <Typography>{day.name}</Typography>
+      <Meal mealName={"Morgenessen"} dish={day.breakfast}/>
+      <Meal mealName={"Mittagessen"} dish={day.lunch}/>
+      <Meal mealName={"Abendessen"} dish={day.dinner}/>
     </Card>
   </>
 }
@@ -27,8 +32,11 @@ function Meal(props: {mealName: string, dish: DishModel}) {
   const handleClick = () => history.push('/dishes');
   let dish = props.dish;
   let mealName = props.mealName;
-  return <Box display="flex" flexDirection="row" flexWrap="nowrap">
-    <Typography>{mealName}</Typography>
-    <Typography onClick={handleClick}>{dish.name}</Typography>
-  </Box>;
+  const classes = useStyles();
+  return <Paper elevation={1} className={classes.mealPaper} onClick={handleClick} >
+    <Box display="flex" flexDirection="column" alignItems="flex-start">
+      <Typography variant="caption">{mealName}</Typography>
+      <Typography variant="h6">{dish.name}</Typography>
+    </Box>
+  </Paper>;
 }
--- frontend/planeat/src/components/order/planning/DayListComponent.tsx
+++ frontend/planeat/src/components/order/planning/DayListComponent.tsx
@@ -6,7 +6,7 @@ import {DishModel} from "../../../models/DishModel";
 export default function DayListComponent() {
   let monday = new DayModel("Montag", new Date(), DishModel.createEmptyDish(), DishModel.createEmptyDish(), DishModel.createEmptyDish())
   let tuesday = new DayModel("Dienstag", new Date(), DishModel.createEmptyDish(), DishModel.createEmptyDish(), DishModel.createEmptyDish())
-  let wendesday = new DayModel("Mittag", new Date(), DishModel.createEmptyDish(), DishModel.createEmptyDish(), DishModel.createEmptyDish())
+  let wendesday = new DayModel("Mittwoch", new Date(), DishModel.createEmptyDish(), DishModel.createEmptyDish(), DishModel.createEmptyDish())
   return (<div>
     <DayComponent day={monday}/>
     <DayComponent day={tuesday}/>
