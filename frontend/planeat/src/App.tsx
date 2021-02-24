a844bfb516a7d3aaad17bbfaf0328562549bb004
marcosteiner <marco.steiner@windowslive.com>
2021-02-24 11:43:25 +0100

    Added card to day component

--- frontend/planeat/src/App.tsx
+++ frontend/planeat/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import PlanningComponent from "./components/order/planning/PlanningComponent";
 
--- frontend/planeat/src/components/order/planning/DayComponent.tsx
+++ frontend/planeat/src/components/order/planning/DayComponent.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import {DayModel} from "../../../models/DayModel";
-import {Typography} from "@material-ui/core";
+import {Card, makeStyles, Typography} from "@material-ui/core";
+
+const useStyles = makeStyles({
+  dayCard: {
+    marginBottom: '5px',
+  },
+});
 
 export default function DayComponent(props: {day: DayModel}) {
+  const classes = useStyles();
   let day = props.day
   return <>
-    <Typography>{day.name}</Typography>
-    <Typography>Morgen:</Typography>
-    <Typography>{day.breakfast.name}</Typography>
-    <Typography>Mittag:</Typography>
-    <Typography>{day.lunch.name}</Typography>
-    <Typography>Abend:</Typography>
-    <Typography>{day.dinner.name}</Typography>
+    <Card className={classes.dayCard}>
+      <Typography>{day.name}</Typography>
+      <Typography>Morgen:</Typography>
+      <Typography>{day.breakfast.name}</Typography>
+      <Typography>Mittag:</Typography>
+      <Typography>{day.lunch.name}</Typography>
+      <Typography>Abend:</Typography>
+      <Typography>{day.dinner.name}</Typography>
+    </Card>
   </>
 }
